e2306bd77d841f46c693b762075bca1b7f4002b6
Ramses Rmz <a00344520@tec.mx>
2022-06-03 01:04:11 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+            /* Si View esta vacio, se retornara la vista cuyo nombre sea igual al nombre del metodo (en este caso Index)... 
+             * mientras que si se le agrega un parametro ("Index2"), se retornara la vista con ese nombre */
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿namespace Portafolio.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; } //para definir que un parametro no sea nulo, se pone un ? 
+        public int Edad { get; set; }
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+    }
+}
